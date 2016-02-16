e77f8ed755090c3f03b38c8670d40a11a1e311aa
Mateusz Wilk <matewilk@gmail.com>
2016-02-16 20:41:49 +0000

    Revert "LRevert "Prevent Angular blinking on app start""
    This reverts commit 5c620776a0367d604ef3fe4ce51ef1f7aee6272d.

--- app/main.js
+++ app/main.js
@@ -31,4 +31,9 @@ module.exports = angular.module('app',[
 .controller('BottomSheetController', bottomSheetController)
 .controller('MobileController', mobileController)
 .controller('ResumeDialogController', resumeDialogController)
-.directive('speedDialMenu', speedDialMenu);
+.directive('speedDialMenu', speedDialMenu)
+//reload the route as the main view is included in ng-include
+//http://stackoverflow.com/questions/16674279/how-to-nest-ng-view-inside-ng-include#answer-20305370
+.run(['$route', function($route)  {
+    $route.reload();
+}]);
